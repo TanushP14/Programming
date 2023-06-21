27e4b5e59d43092c9040a88270bb2faba98bd557
TanushP14 <136456520+TanushP14@users.noreply.github.com>
2023-06-21 22:06:36 +0530

    Update 4 (90% done)

--- OragnicGroceryStore/js/index.js
+++ OragnicGroceryStore/js/index.js
@@ -1,13 +1,26 @@
-const toggle_btn = document.querySelector('.toggle_btn')
-    const toggle_btnIcon = document.querySelector('.toggle_btn i')
-    const dropDownMenu = document.querySelector('.dropdown_menu')
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    toggle_btn.onclick = function (){
-        dropDownMenu.classList.toggle('open')
-        const isOpen = dropDownMenu.classList.contains('open')
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        toggle_btnIcon.classList = isOpen
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-        ? 'fa-solid fa-xmark'
-        : 'fa-solid fa-bars'
-    }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
